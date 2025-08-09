cfb8326e3d2adcd8a206c53f0e442c79cacdea0a
Ashok Krishnan <ashok.krishnan1994@gmail.com>
2025-08-10 00:35:43 +0800

    fullprojectcommit

---
+++ src/gitexercise/rough.java
@@ -0,0 +1,12 @@
+package gitexercise;
+
+public class rough {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is just for GIT reference!");
+		
+	}
+
+}
